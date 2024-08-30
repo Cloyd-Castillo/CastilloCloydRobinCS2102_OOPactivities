4bd0664d91ae4c21cd233a3b6f8cffa1ebadbbad
Cloyd-Castillo <165748822+Cloyd-Castillo@users.noreply.github.com>
2024-08-30 06:37:33 +0000

    I think, Therefore I am.

---
+++ Exercise Sets/Exercise Sets 01/Tempconvert.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tempconvert 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Celsius value: ");
+        float celsius = scanner.nextFloat();
+        float fahrenheit = celsius * 9/5 + 32;
+        System.out.printf("%.2f Celsius is %.2f Fahrenheit%n", celsius, fahrenheit);
+        scanner.close();
+    }
+}
--- Exercise Sets/Exercise Sets 01/Welcome.java
+++ Exercise Sets/Exercise Sets 01/Welcome.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
-class pract1 {
-    public static void main(String[] args) 
-    {
-        System.out.println("Welcome to CS 211: Object-oriented Programming");
-        Scanner input = new Scanner(System.in);
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CS 211: Object-oriented Programming!");
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = Scanner.nextLine();
-        System.out.println("This course will be fun, " + name);
-        input.close();
+        String name = scanner.nextLine();
+        System.out.println("This course will be fun, " + name + "!");
+        scanner.close();
     }
 }
